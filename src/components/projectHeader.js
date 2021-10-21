7a77be0b3e03753988fd00e1ee91a798d1d43966
Karen Palacio <karen.palacio.1994@gmail.com>
2021-10-21 16:42:46 -0300

    wip identidad para desarrollar tranki

--- src/components/projectHeader.js
+++ src/components/projectHeader.js
@@ -8,8 +8,7 @@ const ProjectHeader = () => {
       <h1>Inbipeda</h1>
       <p>Experiencia vibracional</p>
       <p>Noviembre 2021</p>
-      <Identity/>
-      <div>0-0-0-0-0-0-0</div>
+      <div> ~~identidad WIP~~ </div>
     </div>
   );
 };
