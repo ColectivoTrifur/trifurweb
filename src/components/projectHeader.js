234e01654e58509cd2c54450638b00c37dd2b0fe
Karen Palacio <karen.palacio.1994@gmail.com>
2021-10-21 15:31:19 -0300

    El circulo logo inbipeda master of puppets mkultra 1984

--- src/components/projectHeader.js
+++ src/components/projectHeader.js
@@ -6,6 +6,22 @@ const ProjectHeader = () => {
       <h1>Inbipeda</h1>
       <p>Experiencia vibracional</p>
       <p>Noviembre 2021</p>
+      <svg viewBox="0 0 500 500">
+
+      <path id="curve" fill="transparent"
+            d="
+            M 100, 100
+            m -75, 0
+            a 75,75 0 1,0 150,0
+            a 75,75 0 1,0 -150,0
+            "
+      />
+      <text width="500">
+        <textPath alignment-baseline="top" xlinkHref="#curve">
+          Inbipeda - Inbipeda - Inbipeda - Inbipeda - Inbipeda - Inbipeda -
+        </textPath>
+      </text>
+    </svg>
       <div>0-0-0-0-0-0-0</div>
     </div>
   );
