9a38513a40b3cd895d7add0052ec9ca3e9fd4788
Karen Palacio <karen.palacio.1994@gmail.com>
2021-10-24 19:39:23 -0300

    refactor code because.

--- src/components/collaborators.js
+++ src/components/collaborators.js
@@ -2,42 +2,54 @@ import React from "react";
 import Collaborator from "./Collaborator";
 
 const Collaborators = () => {
+  const data = [
+  {
+    "who": "Rosario Perazolo Masjoan",
+    "what": "Lisiada, cineasta y agitadora cultural"
+  },
+  {
+    "who": "Karen Palacio",
+    "what": "live coder, programadora y artista digital"
+  },
+  {
+    "who": "Sol Avila Vazquez",
+    "what": "productora audiovisual y artista visual"
+  },
+  {
+    "who": "Yamo",
+    "what": "diseñador gráfico, productor de contenido multimedia, muralista, gestor cultural, tallerista, vj y tatuador"
+  },
+  {
+    "who": "Carolina Bottani aka Cherryla",
+    "what": "diseñadora gráfica publicitaria, música experimental electrónica en formato live performance"
+  },
+  {
+    "who": "Axel Schneeberger",
+    "what": " Videógrafo, Fotógrafo y artista visual"
+  },
+  {
+    "who": "Hanni Halliday",
+    "what": "Artista audiovisual, gestora y pastelera vegana de Córdoba Capital"
+  },
+  {
+    "who": "Victoria Falco",
+    "what": "arquitecta y artista"
+  },
+  {
+    "who": "Sergio Scotta",
+    "what": "programador, diseñador, productor y artista tecnológico"
+  }
+]
+
   return (
     <div className="collaborators sides bottom-l">
       <h2 className="bottom">Participantes</h2>
-      <Collaborator
-        who="Rosario Perazolo Masjoan"
-        what="Lisiada, cineasta y agitadora cultural"
-      />
-      <Collaborator
-        who="Karen Palacio"
-        what="live coder, programadora y artista digital"
-      />
-      <Collaborator
-        who="Sol Avila Vazquez"
-        what="productora audiovisual y artista visual"
-      />
-      <Collaborator
-        who="Yamo"
-        what="diseñador gráfico, productor de contenido multimedia, muralista, gestor cultural, tallerista, vj y tatuador"
-      />
-      <Collaborator
-        who="Carolina Bottani aka Cherryla"
-        what="diseñadora gráfica publicitaria, música experimental electrónica en formato live performance"
-      />
-      <Collaborator
-        who="Axel Schneeberger"
-        what=" Videografo, Fotografo y artista visual"
-      />
-      <Collaborator
-        who="Hanni Halliday"
-        what="Artista audiovisual, gestora y pastelera vegana de Córdoba Capital"
-      />
-      <Collaborator who="Victoria Falco" what="arquitecta y artistista" />
-      <Collaborator
-        who="Sergio Scotta"
-        what="programador, diseñador, productor y artista tecnológico"
-      />
+      {data.map((person) => 
+          <Collaborator
+            who={person.who}
+            what={person.what}
+          />)
+      }
     </div>
   );
 };
