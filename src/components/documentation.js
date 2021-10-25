f81d476f01e786695938c728f2aac9048d559ec9
Sergio Scotta <sergio.scotta@gmail.com>
2021-10-25 15:57:57 -0300

    Cleaned up a little

--- src/components/documentation.js
+++ src/components/documentation.js
@@ -1,15 +1,10 @@
 import React from "react";
-import image from "../assets/rectangle-holder.png";
-
-import GalleryItem from "./GalleryItem";
 
 const Documentation = () => {
   return (
     <div className="gallery sides bottom-l">
       <h2 className="bottom">Registros</h2>
-      <div className="grid">
-        <GalleryItem source={image} altText="alternative" />
-      </div>
+      <div className="grid"></div>
     </div>
   );
 };
