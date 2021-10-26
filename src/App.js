0cd8c4018f7c3be1b908a0e3f49d00dfda8f9e76
Karen Palacio <karen.palacio.1994@gmail.com>
2021-10-26 15:52:45 -0300

    backgroundwave

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import ProjectHeader from "./components/projectHeader";
 import TitleParagraph from "./components/TitleParagraph";
 import Collaborators from "./components/collaborators.js";
 import Gal from "./components/gallery.js";
+import Wave from "./components/backgroundWave.js";
 import Documentation from "./components/documentation.js";
+import { renderToStaticMarkup } from 'react-dom/server';
 
 function App() {
+    const svgString = encodeURIComponent(renderToStaticMarkup(<Wave/>));
   return (
     <div className="App">
+      <h1
+      style={{
+          backgroundImage: `url("data:image/svg+xml,${svgString}")`
+        }}
+      >
+      </h1>
       <PageHeader />
       <ProjectHeader />
       <TitleParagraph />
---
+++ src/components/backgroundWave.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Wave = () => {
+    console.log("AAAAAAA")
+    return(
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 612.5 792.57">
+          <defs>
+            <style>
+            {`.cls-1 {
+                fill: none;
+                stroke: black;
+                stroke-miterlimit: 10;
+              }`}
+            </style>
+          </defs>
+          <path className="cls-1" d="M0,0C59.25,0,161,102.39,170.69,199.53c10,100.11,100.82,228.41,263.55,306.3C598.66,584.53,605.81,748.24,612,792" transform="translate(0 0.5)" />
+        </svg>
+    )}
+export default Wave;
