6250f41af534988df34e34bf974ba61af476bbed
Karen Palacio <karen.palacio.1994@gmail.com>
2021-11-01 22:35:14 -0300

    probar con otros gradientes

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import TitleParagraph from "./components/TitleParagraph";
 import Collaborators from "./components/collaborators.js";
 import Gal from "./components/gallery.js";
 import Wave from "./components/backgroundWave.js";
+import BackgroundCircles from "./components/backgroundCircles.js";
 import Documentation from "./components/documentation.js";
 import { renderToStaticMarkup } from 'react-dom/server';
 
 function App() {
     const svgString = encodeURIComponent(renderToStaticMarkup(<Wave/>));
+    const svgString2 = encodeURIComponent(renderToStaticMarkup(<BackgroundCircles/>));
   return (
     <div className="App grid">
-      <h1
-      style={{
-          backgroundImage: `url("data:image/svg+xml,${svgString}")`
-        }}
-      >
-      </h1>
       <PageHeader />
       <ProjectHeader />
       <TitleParagraph />
       <Collaborators />
       <Gal />
+      <div 
+      style={{
+          backgroundImage: `url("data:image/svg+xml,${svgString2}")`
+        }}
+      >
+      </div>
     </div>
   );
 }
---
+++ src/components/backgroundCircles.js
@@ -0,0 +1,166 @@
+import * as React from "react"
+
+function BackgroundCircles(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 203.79 615.45"
+      {...props}
+    >
+      <defs>
+        <radialGradient
+          id="prefix__a"
+          cx={101.9}
+          cy={180.03}
+          r={101.9}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0} stopColor="#ed2590" stopOpacity={0.95} />
+          <stop offset={0.67} stopColor="#f9f06c" stopOpacity={0.65} />
+          <stop offset={0.73} stopColor="#ebb06b" stopOpacity={0.46} />
+          <stop offset={0.79} stopColor="#e07b6a" stopOpacity={0.29} />
+          <stop offset={0.84} stopColor="#d75269" stopOpacity={0.16} />
+          <stop offset={0.9} stopColor="#d03468" stopOpacity={0.07} />
+          <stop offset={0.95} stopColor="#cc2268" stopOpacity={0.02} />
+          <stop offset={1} stopColor="#cb1c68" stopOpacity={0} />
+        </radialGradient>
+        <radialGradient
+          id="prefix__b"
+          cx={101.9}
+          cy={513.56}
+          r={101.9}
+          xlinkHref="#prefix__a"
+        />
+        <radialGradient
+          id="prefix__c"
+          cx={101.9}
+          cy={440.06}
+          r={101.9}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0} stopColor="#ed2590" stopOpacity={0.95} />
+          <stop offset={0.67} stopColor="#a4ce39" stopOpacity={0.65} />
+          <stop offset={0.7} stopColor="#aab240" stopOpacity={0.55} />
+          <stop offset={0.76} stopColor="#b67d4f" stopOpacity={0.35} />
+          <stop offset={0.83} stopColor="#bf525a" stopOpacity={0.2} />
+          <stop offset={0.89} stopColor="#c63462" stopOpacity={0.09} />
+          <stop offset={0.95} stopColor="#ca2266" stopOpacity={0.02} />
+          <stop offset={1} stopColor="#cb1c68" stopOpacity={0} />
+        </radialGradient>
+        <radialGradient
+          id="prefix__d"
+          cx={101.9}
+          cy={349.25}
+          r={101.9}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0} stopColor="#ed2590" stopOpacity={0.95} />
+          <stop offset={0.67} stopColor="#44459c" stopOpacity={0.65} />
+          <stop offset={0.72} stopColor="#6b398d" stopOpacity={0.46} />
+          <stop offset={0.8} stopColor="#952d7d" stopOpacity={0.26} />
+          <stop offset={0.87} stopColor="#b32371" stopOpacity={0.12} />
+          <stop offset={0.94} stopColor="#c51e6a" stopOpacity={0.03} />
+          <stop offset={1} stopColor="#cb1c68" stopOpacity={0} />
+        </radialGradient>
+        <radialGradient
+          id="prefix__e"
+          cx={101.9}
+          cy={295.97}
+          r={101.9}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0} stopColor="#ed2590" stopOpacity={0.95} />
+          <stop offset={0.67} stopColor="#488fcd" stopOpacity={0.65} />
+          <stop offset={0.7} stopColor="#5c7dbe" stopOpacity={0.55} />
+          <stop offset={0.76} stopColor="#845b9f" stopOpacity={0.36} />
+          <stop offset={0.83} stopColor="#a33f87" stopOpacity={0.2} />
+          <stop offset={0.89} stopColor="#b92c76" stopOpacity={0.09} />
+          <stop offset={0.95} stopColor="#c6206c" stopOpacity={0.02} />
+          <stop offset={1} stopColor="#cb1c68" stopOpacity={0} />
+        </radialGradient>
+        <radialGradient
+          id="prefix__f"
+          cx={101.9}
+          cy={101.9}
+          r={101.9}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0.03} stopColor="#6fc7b2" stopOpacity={0.65} />
+          <stop offset={0.15} stopColor="#909da9" stopOpacity={0.73} />
+          <stop offset={0.3} stopColor="#b172a0" stopOpacity={0.81} />
+          <stop offset={0.45} stopColor="#cb5099" stopOpacity={0.87} />
+          <stop offset={0.59} stopColor="#de3894" stopOpacity={0.91} />
+          <stop offset={0.72} stopColor="#e92a91" stopOpacity={0.94} />
+          <stop offset={0.84} stopColor="#ed2590" stopOpacity={0.95} />
+          <stop offset={1} stopColor="#cb1c68" stopOpacity={0} />
+        </radialGradient>
+      </defs>
+      <g data-name="Layer 1">
+        <circle
+          cx={101.9}
+          cy={180.03}
+          r={101.9}
+          style={{
+            mixBlendMode: "overlay",
+          }}
+          fill="url(#prefix__a)"
+          opacity={0.78}
+        />
+        <circle
+          cx={101.9}
+          cy={513.56}
+          r={101.9}
+          style={{
+            mixBlendMode: "overlay",
+          }}
+          fill="url(#prefix__b)"
+          opacity={0.78}
+        />
+        <circle
+          cx={101.9}
+          cy={440.06}
+          r={101.9}
+          style={{
+            mixBlendMode: "overlay",
+          }}
+          fill="url(#prefix__c)"
+          opacity={0.78}
+        />
+        <circle
+          cx={101.9}
+          cy={349.25}
+          r={101.9}
+          style={{
+            mixBlendMode: "overlay",
+          }}
+          fill="url(#prefix__d)"
+          opacity={0.78}
+        />
+        <circle
+          cx={101.9}
+          cy={295.97}
+          r={101.9}
+          style={{
+            mixBlendMode: "overlay",
+          }}
+          fill="url(#prefix__e)"
+          opacity={0.78}
+        />
+        <circle
+          cx={101.9}
+          cy={101.9}
+          r={101.9}
+          style={{
+            mixBlendMode: "overlay",
+          }}
+          fill="url(#prefix__f)"
+          opacity={0.78}
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default BackgroundCircles;
+
