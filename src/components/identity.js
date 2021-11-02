709d8ae86a47683a5e56f8962055d9609fa7fe6e
Sergio Scotta <sergio.scotta@gmail.com>
2021-11-01 23:25:33 -0300

    Fixed logo

--- src/components/identity.js
+++ src/components/identity.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-import Gradient from './gradient.js';
-import LogoCircle from './logoCircle.js';
+import Gradient from "./gradient.js";
+import LogoCircle from "./logoCircle.js";
 
-const Identity = () => { 
-    return (
-        <div className="identity">
-            <Gradient cx="89.48" opacity=".9" offset=".82"/>
-            <LogoCircle />
-        </div>
-    );
+const Identity = () => {
+  return (
+    <div className="identity">
+      <div className="svg-container">
+        <Gradient cx="89.48" opacity=".9" offset=".82" />
+        <LogoCircle />
+      </div>
+    </div>
+  );
 };
 export default Identity;
